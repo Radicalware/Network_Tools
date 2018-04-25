e54d40864b5e0608f75083f2c7c666cdac045002
Brandon Forbes <reznok@gmail.com>
2018-04-24 17:19:13 -0700

    Support for "import Suricata_Parser"

--- pSuricata/psuricata.py
+++ pSuricata/psuricata.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import os,sys
-import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0) # line by DEKHTIARJonathan 
+import fcntl; fcntl.fcntl(1, fcntl.F_SETFL, 0) # line by DEKHTIARJonathan
 # fcntl is for users who have multi-line PS1 output
 
-from Suricata_Parser import Suricata_Parser, HTTP_c, Fast_c
+import Suricata_Parser
 
 def helper(white, green):
 	print (white +'''
@@ -17,7 +17,7 @@ def helper(white, green):
 
 	usage: '''+green+'''
 	$> psuricata.py -r pcap_file_name.pcap Exploit "-> 192.168.204.137"'''+white+'''
-	
+
 	Use the command above if you don't have logs stored for that pcap already
 	The command will search for if either "Exploit" or "-> 192.168.204.137" pops
 	'''+green+'''
@@ -28,14 +28,14 @@ def helper(white, green):
 	also, there is a '-v' which means that the timestamp 18:19:24.107376
 	will be omitted. The '-g' was needed because we had a '-v' in use.
 
-	arguments: 
+	arguments:
 	-r <pcap file>   = use this to update or parse your suricata pcap data
 	-a               = Grep uses results from bool 'and' instead of default bool 'or'
 	-g               = Greps the trailing array args until the next '-v'
 	-v               = vGreps the trailing array args until the next '-g'
 	-u               = Searches only for querys that have a url (HTTP traffic)
 
-	That command will show the results that include the two arguments 
+	That command will show the results that include the two arguments
 	in the suricata output as an and statment.
 
 	Be sure to make sure your interface matches and the http is enabled
@@ -48,7 +48,7 @@ def helper(white, green):
 	https://blog.rapid7.com/2017/02/14/how-to-install-suricata-nids-on-ubuntu-linux/'''+white+'''
 	http://suricata.readthedocs.io/en/latest/install.html#install-binary-packages
 
-	Note: search color highlighting is Case Sensitive, however, 
+	Note: search color highlighting is Case Sensitive, however,
 		  the grep search itself is not case sensitive.
 
 			''');quit()
@@ -60,7 +60,7 @@ def f(): # use this for debugging
 	quit()
 
 
-http_log = HTTP_c.HTTP_c()
+http_log = Suricata_Parser.HTTP_c()
 #          FileName.ClassName
 http_log.parse_http()
 
